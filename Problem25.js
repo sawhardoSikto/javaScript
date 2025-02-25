6da0c6e878684ee2c35ac8e3c18fb0edc0bb7fa4
sawhardoSikto <siktobiswas@gmail.com>
2025-02-25 23:13:25 +0600

    25 number problem added

---
+++ Problem25.js
@@ -0,0 +1,19 @@
+function noDuplicate(numbers)
+
+{
+    const arr=[];
+    for (let number of numbers)
+    {
+        if(arr.includes(number)==false)
+        {
+            arr.push(number);
+
+        }
+    }
+   return arr;
+
+
+}
+let value =[5,5,3,2,5,7,3];
+const removeDuplicate=noDuplicate(value);
+console.log(removeDuplicate);
