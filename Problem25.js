9213eb8d9cd00f4b8b6875d187dd185dcc735231
sawhardoSikto <siktobiswas@gmail.com>
2025-02-25 23:14:16 +0600

    25 number problem edited

--- Problem25.js
+++ Problem25.js
@@ -4,6 +4,7 @@ function noDuplicate(numbers)
     const arr=[];
     for (let number of numbers)
     {
+        console.log(number);
         if(arr.includes(number)==false)
         {
             arr.push(number);
@@ -17,3 +18,4 @@ function noDuplicate(numbers)
 let value =[5,5,3,2,5,7,3];
 const removeDuplicate=noDuplicate(value);
 console.log(removeDuplicate);
+
