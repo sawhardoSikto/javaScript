f5ae92684bba8242b586dc6b88a26e985c574cbd
sawhardoSikto <siktobiswas@gmail.com>
2025-03-01 14:17:40 +0600

    Update Problem25.js

--- Problem25.js
+++ Problem25.js
@@ -83,3 +83,112 @@ function generateRandomNumber()
 
 }
 console.log(generateRandomNumber());
+
+
+console.log("____________________________________");
+
+function lowestnumber(numbers)
+{
+    let lower=numbers[0];
+    for (let number of numbers)
+    {
+        if(number<lower)
+        {
+            lower=number;
+
+        }
+
+
+    }
+    return lower;
+}
+
+const heights2 = [167, 190, 120, 165, 137];
+
+console.log(lowestnumber(heights2));
+
+console.log("__________________________________________");
+
+function smallestName(names)
+{
+    let lower=names[0];
+    for(let name of names)
+    {
+        if(name.length<lower.length)
+        {
+            lower=name;
+        }
+    }
+    return lower
+}
+
+const heights3 = ['rahim', 'robin', 'rafi', 'ron', 'rashed'];
+console.log(smallestName(heights3));
+
+console.log("________________________");
+
+
+function totalBudget(laptopQuan,tabletQuan,mobileQuan)
+{
+   const laptop = 35000 ;
+    const tablet = 15000 ;
+    const mobile = 20000 ;
+
+    let laptopPrice=laptop*laptopQuan;
+    let tabletPrice=tablet*tabletQuan;
+    let mobilePrice=mobile*mobileQuan
+    let totalPrice=laptopPrice+tabletPrice+mobilePrice;
+    return totalPrice
+    
+}
+console.log(totalBudget(5,5,6));
+
+console.log("______________________________");
+
+function findAveragePhonePrice(arrayOfObject,size)
+{
+    let sum=0;
+    for(let number of arrayOfObject)
+    {
+         sum=sum+ number.price;
+    }
+    let avg=sum/size;
+    return avg.toFixed(2);
+
+}
+
+const phones = [
+    { model: "PhoneA", brand: "Iphone", price: 95000 },
+    { model: "PhoneB", brand: "Samsung", price: 40000 },
+    { model: "PhoneC", brand: "Oppo", price: 26000 },
+    { model: "PhoneD", brand: "Nokia", price: 35000 },
+    { model: "PhoneE", brand: "Iphone", price: 105000 },
+    { model: "PhoneF", brand: "HTC", price: 48000 },
+];
+const size=phones.length
+console.log(findAveragePhonePrice(phones,size));
+
+
+console.log("_____________________");
+
+function totalSalary(arrayOfObject)
+{
+    let abc=0
+    for (let num of arrayOfObject )
+        {
+           abc+=(num.starting+(num.experience*num.increment));
+
+        } 
+        return abc
+}
+
+const employees = [
+    { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
+    { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
+    { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
+    { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
+];
+
+console.log(totalSalary(employees));
+
+
